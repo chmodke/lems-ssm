744f2e9a5ff0733aa1d0060c56466197e03514cb
chmodke <kehao1158115@outlook.com>
2017-04-30 15:41:37 +0800

    init

--- src/main/java/org/kehao/lems/dao/EquLabMapper.java
+++
@@ -1,9 +0,0 @@
-package org.kehao.lems.dao;
-
-import org.kehao.lems.entity.EquLab;
-
-public interface EquLabMapper {
-    int insert(EquLab record);
-
-    int insertSelective(EquLab record);
-}
--- src/main/java/org/kehao/lems/dao/RolePermissionMapper.java
+++
@@ -1,9 +0,0 @@
-package org.kehao.lems.dao;
-
-import org.kehao.lems.entity.RolePermission;
-
-public interface RolePermissionMapper {
-    int insert(RolePermission record);
-
-    int insertSelective(RolePermission record);
-}
